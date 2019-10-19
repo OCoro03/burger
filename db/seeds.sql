a9ef0a972dd8ffaf26e46e22264bd4174dc07d4f
Orlando Corona <corona.orlando@gmail.com>
2019-10-19 09:04:00 -0500

    worked on the whole Hw

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Double Cheese', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mac & Cheese Burger', true);
